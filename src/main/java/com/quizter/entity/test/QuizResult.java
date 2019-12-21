6a080d92110df412b3a6214733d623fe42a77ae2
Vasyl Lelek <itevas@outlook.com>
2019-12-21 16:03:26 +0200

    fixed some issues

--- src/main/java/com/quizter/entity/test/QuizResult.java
+++ src/main/java/com/quizter/entity/test/QuizResult.java
@@ -6,7 +6,13 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.time.Instant;
 import java.util.List;
 
